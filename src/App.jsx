3b7f7a4d2660797201e41409852bfe9c76d83c82
Khanh <dinhvankhanh20051983@gmail.com>
2026-04-18 01:46:28 +0700

    fix supabase import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from './lib/supabaseClient';
+import { supabase } from './lib/supabaseClient.js';
 
 import RoleSelect from './components/RoleSelect';
 import LoginOwner from './components/LoginOwner';
